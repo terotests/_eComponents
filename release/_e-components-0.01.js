442e5eb75fa444658567512b3960b61ce7d25bc1
Tero Tolonen <digikansio@gmail.com>
2015-09-28 21:56:43 +0300

    "commit message"

---
+++ release/_e-components-0.01.js
@@ -0,0 +1,326 @@
+// The code template begins here
+"use strict";
+
+(function () {
+
+  var __amdDefs__ = {};
+
+  // The class definition is here...
+  var _eComponents_prototype = function _eComponents_prototype() {
+    // Then create the traits and subclasses for this class here...
+
+    (function (_myTrait_) {
+
+      // Initialize static variables here...
+
+      if (_myTrait_.__traitInit && !_myTrait_.hasOwnProperty("__traitInit")) _myTrait_.__traitInit = _myTrait_.__traitInit.slice();
+      if (!_myTrait_.__traitInit) _myTrait_.__traitInit = [];
+      _myTrait_.__traitInit.push(function (options) {
+        var body = _e(document.body);
+
+        this.materialComps(body);
+      });
+
+      /**
+       * @param Object body  - _e() object to initialize to
+       */
+      _myTrait_.materialComps = function (body) {
+
+        var paper_input = function paper_input(scope, name, baseColor, elemName) {
+          if (!elemName) elemName = "input";
+          scope.customElement(name, {
+            data: {
+              title: "input title",
+              value: "the value"
+            },
+            css: function css(myCss) {
+              myCss.bind(".input-container", {
+                "margin-top": "0.2em",
+                "margin-bottom": "0.2em",
+                "margin-right": "0.2em"
+              });
+              myCss.bind(".paper-input", {
+                "border-radius": "0",
+                "width": "100%",
+                "font-size": "1em",
+                "outline": "none",
+                "border": "none",
+                "border-bottom": "1px solid #757575",
+                "padding": "10px 10px 10px 5px",
+                "display": "block",
+                "background-color": "#fafafa"
+              });
+              myCss.bind(".box", {
+                width: "100%",
+                height: "2px",
+                "background-color": _e().mix("#4a89dc", baseColor),
+                transform: "scale(0,1)",
+                "opacity": 1
+              });
+              myCss.animation("input-enter", {
+                duration: "0.5s",
+                "iteration-count": 1 }, {
+                width: "100%",
+                background: "#fff",
+                opacity: 1
+              }, 0.3, {
+                width: "100%",
+                background: _e().mix(baseColor, "white", 0.7),
+                opacity: 1
+              }, {
+                width: "100%",
+                background: "#fff",
+                opacity: 1
+              });
+              myCss.animation("input-bar-enter", {
+                duration: "0.5s",
+                "iteration-count": 1 }, {
+                opacity: 0,
+                transform: "scale(0, 1)"
+              }, {
+                opacity: 1,
+                transform: "scale(1, 1)"
+              }, {
+                duration: 0.5
+              });
+              myCss.animation("input-bar-close", {
+                duration: "0.5s",
+                "iteration-count": 1 }, {
+                opacity: 0,
+                transform: "scale(1, 1)"
+              }, {
+                opacity: 1,
+                transform: "scale(0, 1)"
+              }, {
+                duration: 0.5
+              });
+              myCss.bind(".entered", {
+                transform: "scale(1,1)"
+              });
+              myCss.bind(".paper-input-title", {
+                opacity: 0.6
+              });
+              myCss.bind(".titleFocused", {
+                opacity: 0.8
+              });
+            },
+            init: function init(data) {
+              var box;
+              var container = this.div("input-container");
+              var title = container.div("paper-input-title").bind(data, "title");
+              var input = container[elemName]("paper-input");
+
+              input.bind(data, "value");
+              input.on("focus", function () {
+                box.removeClass("input-bar-close");
+                box.addClass("input-bar-enter");
+                box.addClass("entered");
+                this.addClass("input-enter");
+                title.addClass("titleFocused");
+              }).on("blur", function () {
+                box.removeClass("input-bar-enter");
+                box.addClass("input-bar-close");
+                box.removeClass("entered");
+                this.removeClass("input-enter");
+                title.removeClass("titleFocused");
+              });
+              var box = container.div("box");
+            },
+            tagName: "div"
+          });
+        };
+        paper_input(body, "paper-input", "orange");
+        paper_input(body, "paper-textarea", "red", "textarea");
+
+        // material design circle effect component, which removes itself automatically
+        var paper_circle = function paper_circle(scope, name, size, baseColor, duration) {
+          scope.customElement(name, {
+            css: function css(myCss) {
+              duration = duration || 0.6;
+              myCss.bind(".circle", {
+                "border-radius": "50%",
+                "width": size,
+                "height": size,
+                "transform": "translate(-50%, -50%)",
+                "position": "absolute",
+                "top": 0,
+                "left": 0,
+                "background-color": baseColor
+              });
+              myCss.animation("animate", {
+                duration: duration + "s",
+                "iteration-count": 1 }, {
+                opacity: 0,
+                transform: "scale(0,0)"
+              }, 0.1, {
+                opacity: 1,
+                transform: "scale(0.3,0.3)"
+              }, 0.4, {
+                transform: "scale(0.7,0.7)",
+                opacity: 0.8
+              }, {
+                transform: "scale(1,1)",
+                opacity: 0
+              });
+            },
+            init: function init() {
+              var container = _e("div");
+              container.relative();
+              this.prepend(container);
+              var rel = container.div().absolute();
+              var m = this.mousePos();
+              rel.x(m.x).y(m.y);
+              rel.div("circle");
+              rel.addClass("animate");
+              var me = this;
+              setTimeout(function () {
+                container.remove();
+              }, duration * 1000);
+            },
+            tagName: "div"
+          });
+        };
+        paper_circle(body, "paper-circle", "200px", "rgba(255, 255, 255, 0.25)");
+        paper_circle(body, "paper-circle-red", "200px", "rgba(255, 0, 0, 0.25)");
+        paper_circle(body, "paper-circle-green", "200px", "rgba(0, 255, 0, 0.25)");
+        paper_circle(body, "paper-circle-dark", "200px", "rgba(0, 0, 0, 0.15)");
+        paper_circle(body, "paper-circle-huge", "1200px", "rgba(255, 255, 255, 0.12)", 1.4);
+
+        var create_btn = function create_btn(scope, name, color, borderRadius) {
+          scope.customElement(name, {
+            data: {
+              text: "Button text"
+            },
+            css: function css(myCss) {
+              var btnShadow = "0 3px 10px rgba(0, 0, 0, 0.34)";
+              var myColor = _e().mix("#4a89dc", color);
+              myCss.bind(".btn-content", {
+                "display": "inline-block",
+                "padding": "0.4em 0.8em",
+                "position": "relative",
+                "margin": "0.3em",
+                "overflow": "hidden",
+                "cursor": "pointer",
+                "color": "#fff",
+                "border-radius": borderRadius || 0,
+                "background-color": myColor,
+                "box-shadow": btnShadow
+              });
+              myCss.bind(".btn-content:hover", {
+                "background": _e().dim(_e().mix(color, "#4a89dc"), 0.1)
+              });
+              myCss.animation("tryMe", {
+                duration: "4s",
+                "iteration-count": 1 }, {
+                transform: "rotate(0deg)"
+              }, {
+                transform: "rotate(360deg)"
+              });
+            },
+            init: function init(data, createOptions) {
+              this.addClass("btn-content");
+              this.span().bind(data, "text");
+              this.on("click", function () {
+                this.e("paper-circle");
+              });
+            },
+            tagName: "div"
+          });
+        };
+        create_btn(body, "pri-button", "gray", "0px");
+        create_btn(body, "danger-button", "red", "0px");
+        create_btn(body, "warning-button", "yellow", "0px");
+
+        var paper_heading = function paper_heading(scope, name, size, baseColor) {
+          scope.customElement(name, {
+            // The data-model for the component
+            data: {
+              text: "Default heading for the component"
+            },
+            css: function css(myCss) {
+              myCss.bind(".h1-container", {
+                "margin-top": "0.1em",
+                "margin-bottom": "0.2em",
+                "margin-right": "0.2em",
+                "cursor": "pointer",
+                "font-size": size,
+                "opacity": 0.9,
+                "color": _e().mix("#4a89dc", baseColor, 0.6)
+              });
+              myCss.bind(".box", {
+                width: "100%",
+                height: "2px",
+                "transform-origin": "100% 0",
+                "background-color": _e().mix("#4a89dc", baseColor),
+                transform: "scale(1,1)",
+                "margin-bottom": "0.4em",
+                "opacity": 0.5
+              });
+              myCss.animation("box-enter", {
+                duration: "1s",
+                "iteration-count": 1 }, {
+                opacity: 0,
+                transform: "scale(0, 1)"
+              }, {
+                opacity: 0.5,
+                transform: "scale(1, 1)"
+              }, {
+                duration: 0.5
+              });
+            },
+            init: function init(data) {
+              this.div("h1-container").bind(data, "text");
+              var box = this.div("box").addClass("box-enter");
+              this.on("click", function () {
+                box.removeClass("box-enter");
+                var me = this;
+                setTimeout(function () {
+                  box.addClass("box-enter");
+                }, 100);
+              });
+            },
+            tagName: "div"
+          });
+        };
+        // create component for paper -like h1
+        paper_heading(body, "paper-h1", "2em", "#333");
+        paper_heading(body, "paper-h2", "1.5em", "#666");
+      };
+    })(this);
+  };
+
+  var _eComponents = function _eComponents(a, b, c, d, e, f, g, h) {
+    var m = this,
+        res;
+    if (m instanceof _eComponents) {
+      var args = [a, b, c, d, e, f, g, h];
+      if (m.__factoryClass) {
+        m.__factoryClass.forEach(function (initF) {
+          res = initF.apply(m, args);
+        });
+        if (typeof res == "function") {
+          if (res._classInfo.name != _eComponents._classInfo.name) return new res(a, b, c, d, e, f, g, h);
+        } else {
+          if (res) return res;
+        }
+      }
+      if (m.__traitInit) {
+        m.__traitInit.forEach(function (initF) {
+          initF.apply(m, args);
+        });
+      } else {
+        if (typeof m.init == "function") m.init.apply(m, args);
+      }
+    } else return new _eComponents(a, b, c, d, e, f, g, h);
+  };
+  // inheritance is here
+
+  _eComponents._classInfo = {
+    name: "_eComponents"
+  };
+  _eComponents.prototype = new _eComponents_prototype();
+
+  if (typeof define !== "undefined" && define !== null && define.amd != null) {
+    define(__amdDefs__);
+  }
+}).call(new Function("return this")());
