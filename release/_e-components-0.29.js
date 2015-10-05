702615681c932c12ffbd3804e5b525b0a62d93ee
Tero Tolonen <digikansio@gmail.com>
2015-10-05 23:14:38 +0300

    "commit message"

--- release/_e-components-0.29.js
+++ release/_e-components-0.29.js
@@ -497,7 +497,7 @@
 
             var model = this.state();
 
-            var model = _data(data.get("dataid"));
+            // var model = _data(data.get("dataid"));
             var level_1 = data.get("firstLevel");
             var level_2 = data.get("secondLevel");
             var default_icon = data.get("defaultIcon");
